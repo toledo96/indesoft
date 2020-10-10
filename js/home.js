10bbc166dbf8b040b5d168ee06a1ccf6171eb02b
unknown <atm1996.ast@gmail.com>
2020-10-09 22:52:31 -0500

    first commit

---
+++ js/home.js
@@ -0,0 +1,14 @@
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x .style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+$(document).ready(function(){
+    $("#show").click(function(){
+        $("#myDIV").toggle();
+    });
+});
